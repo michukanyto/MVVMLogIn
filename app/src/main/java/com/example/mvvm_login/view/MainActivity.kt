8cc78896b37e407bfdf411e9c1ea6f94b78bea44
michukanyto <escobar.marlon@gmail.com>
2019-12-07 16:00:42 -0500

    viewModel implemented!

--- app/src/main/java/com/example/mvvm_login/view/MainActivity.kt
+++ app/src/main/java/com/example/mvvm_login/view/MainActivity.kt
@@ -2,16 +2,37 @@ package com.example.mvvm_login.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.example.mvvm_login.R
 import com.example.mvvm_login.viewmodel.LogInViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: LogInViewModel
+    private var user:String? = null
+    private var pass:String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        user = userEditText.text.toString()
+        pass = passEditText.text.toString()
+
+
+        viewModel = ViewModelProviders.of(this).get(LogInViewModel::class.java)
+
+        viewModel.validateCredentials(user!!,pass!!).observe(this, Observer { logIn ->
+            if (logIn.logInOK){
+                Toast.makeText(this,"New Activity Loading",Toast.LENGTH_LONG).show()
+                logInButton.visibility = View.INVISIBLE
+            } else {
+                messageTextView.visibility = View.VISIBLE
+            }
+        })
 
 
     }
--- app/src/main/java/com/example/mvvm_login/viewmodel/LogInViewModel.kt
+++ app/src/main/java/com/example/mvvm_login/viewmodel/LogInViewModel.kt
@@ -7,10 +7,15 @@ import com.example.mvvm_login.model.LogIn
 
 class LogInViewModel : ViewModel() {
 
+    private val USERNAME = "admin"
+    private val PASSWORD = "pass"
+
     private val logIn = MutableLiveData<LogIn>()
 
     fun validateCredentials(user:String,password:String): LiveData<LogIn> {
+        var isItOK:Boolean = false
+        isItOK = user == USERNAME && password == PASSWORD
+        logIn.value = LogIn(user,password,isItOK)
         return logIn
-
     }
 }
